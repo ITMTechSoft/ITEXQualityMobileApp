ac45de6a618083075375aba3a060ed00f51ee7cc
Monzer <Monzer.al.khiami@itm-techsoft.com>
2021-07-06 13:45:36 +0300

    Start Final Control

---
+++ lib/Screens/Home/QualityTest/FinalControl/FinalControl.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class FinalControl extends StatefulWidget {
+
+  FinalControl();
+
+  @override
+  _FinalControlState createState() => _FinalControlState();
+}
+
+class _FinalControlState extends State<FinalControl> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
